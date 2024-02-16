123b35ac200b9c4f8ddc8d0b821422569690acaf
Mohd Daniyal <mohddaniyalh@gmail.com>
2024-02-17 01:12:51 +0530

    Adding code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import LanguageCard from './components/LanguageCard';
 
-function App() {
+const App = () => {
+  const [languages, setLanguages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL + '/languages')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const parsedData = data.body ? JSON.parse(data.body) : data;
+        if (Array.isArray(parsedData)) {
+          setLanguages(parsedData);
+        } else {
+          console.error('Invalid data format received:', parsedData);
+        }
+      })
+      .catch(error => console.error('Error fetching data:', error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const handleVote = language => {
+    fetch(process.env.REACT_APP_API_URL + `/languages/${language}/vote`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        operation: 'update',
+        payload: { language },
+      }),
+    })
+      .then(response => response.json())
+      .then(() => {
+        fetch(process.env.REACT_APP_API_URL + '/languages')
+          .then(response => response.json())
+          .then(data => {
+            const parsedData = data.body ? JSON.parse(data.body) : data;
+            if (Array.isArray(parsedData)) {
+              setLanguages(parsedData);
+            } else {
+              console.error('Invalid data format received:', parsedData);
+            }
+          })
+          .catch(error => console.error('Error fetching data:', error));
+      })
+      .catch(error => console.error('Error updating vote:', error));
+  };  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container mx-auto">
+      <div class="flex justify-center items-center h-full mt-8">
+        <p class="font-bold text-3xl text-center">
+          Vote for your 2024
+          <span class="text-gray-700 mx-1 font-extrabold text-4xl relative inline-block stroke-current">
+            Favourite
+            <svg class="absolute -bottom-0.5 w-full max-h-1.5" viewBox="0 0 55 5" xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none">
+              <path d="M0.652466 4.00002C15.8925 2.66668 48.0351 0.400018 54.6853 2.00002" stroke-width="2"></path>
+            </svg>
+          </span>
+          language!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0">
+        {languages.map(language => (
+          <LanguageCard key={language.Language} language={language} handleVote={handleVote} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/LanguageCard.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const LanguageCard = ({ language, handleVote }) => {
+  return (
+    <div
+      key={language.Language}
+      className="min-h-80 gap-6 flex items-center justify-center"
+    >
+      <div className="bg-gray-100 relative shadow-xl overflow-hidden hover:shadow-2xl group rounded-xl p-5 transition-all duration-500 transform">
+        <div className="flex items-center gap-4">
+          <img src={language.Image}
+            alt={language.Name}
+            className="w-32 group-hover:w-36 group-hover:h-36 h-32 object-center object-cover transition-all duration-500 delay-500 transform"
+          />
+          <div className="w-fit transition-all transform duration-500">
+            <h1 className="text-gray-600 font-bold">
+              {language.Name}
+            </h1>
+            <a
+              href={language.Documentation}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:underline hover:text-black"
+            >
+              View Reference
+            </a>
+            <a
+              className="block text-xs text-gray-500 group-hover:opacity-100 opacity-0 transform transition-all delay-300 duration-500 mt-2"
+            >
+              {language.Votes} Votes
+            </a>
+          </div>
+        </div>
+        <button
+          onClick={() => handleVote(language.Language)}
+          className="absolute bottom-1 right-1 bg-gray-500 rounded-xl p-1 w-20 h-9 duration-500 hover:bg-gray-700 transition-all group-hover:right-1 opacity-0 group-hover:opacity-100 text-white flex items-center justify-center"
+        >
+          +1
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LanguageCard;
